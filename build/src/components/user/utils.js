f179df28914e75b4624e53334778ce0e8d80d302
Said1703 <said-drk@hotmail.com>
2024-01-12 20:41:34 -0500

    js

---
+++ build/src/components/user/utils.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.mapInsertProduct = void 0;
+function mapInsertProduct(body) {
+    const { product, category } = body;
+}
+exports.mapInsertProduct = mapInsertProduct;
