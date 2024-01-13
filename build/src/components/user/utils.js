582dcb39f9c3c761bf69b767f6f5361ed98c52f3
Said1703 <said-drk@hotmail.com>
2024-01-12 22:00:30 -0500

    borrado

--- build/src/components/user/utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mapInsertProduct = void 0;
-function mapInsertProduct(body) {
-    const { product, category } = body;
-}
-exports.mapInsertProduct = mapInsertProduct;
